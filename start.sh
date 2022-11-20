db3e0af8224c8ca097d33e05b798db9534cf2071
kumaranmark <116642345+kumaranmark@users.noreply.github.com>
2022-11-20 16:34:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/200920082007/TigerShroff.git /TigerShroff
-cd /TigerShroff
+git clone -b main https://github.com/kumaranmark/tom.git /TigerShroff
+cd /Tom
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
